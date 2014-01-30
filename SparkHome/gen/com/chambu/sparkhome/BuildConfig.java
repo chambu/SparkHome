d6b6bb8447d289089073174d106edbd3c6016282
chambu <gowthamj@gmail.com>
2014-01-30 15:25:40 -0500

    test Commit

---
+++ SparkHome/gen/com/chambu/sparkhome/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chambu.sparkhome;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SparkHome/src/com/chambu/sparkhome/SparkMain.java
@@ -0,0 +1,22 @@
+package com.chambu.sparkhome;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SparkMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_spark_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.spark_main, menu);
+		return true;
+	}
+
+}
